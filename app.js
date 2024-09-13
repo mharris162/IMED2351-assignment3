26595a1ad389935fb1523538b9d08528e5a1e82b
mharris162 <mharris162@student.alamo.edu>
2024-09-12 22:56:50 -0500

    event

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const name = "mackenzie"; // global scope; can be access outside of function
 
-function logger() {   
+//function 1
+function logger() {     //content inside () are paramters 
     console.log(name);
     console.log("Hi");
     console.log("Hi");
@@ -11,17 +12,33 @@ function logger() {
 
 logger();
 
-
+//function 2
 function adder (num1, num2) {
     console.log(num1 + num2);
 }
 
 adder(4,4);
 
-
+//function 3
 function toUpper(text) {
     const upperCased = text.toUpperCase();
     console.log(upperCased);
 }
 
 toUpper(name);
+
+//object
+const cat = {
+// object properties 
+    name: "Louis",
+    type: "cat",
+    age: "9",
+    color: "Tuxedo",
+    ownerLastName: "Harris",
+//Object Method
+    FullName: function (){
+        return this.name + " " + this.ownerLastName;
+    }
+};
+
+console.log(cat);
